3994494e5330f250fbe3172b26d31329b3cd2051
es <emma@huron.local>
2023-12-04 07:51:36 -0700

    updated helper scripts

--- get_inventory_data.sh
+++ get_inventory_data.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-/opt/clmgr/bin/cm inventory -e -j > '/home/emma/de-inventory-tracker/data/'$(date "+de-%Y%m%d%H%M")
+/opt/clmgr/bin/cm inventory -e -j > '/home/emma/HPCM-inventory-tracker/data/'$(date "+de-%Y%m%d%H%M")
